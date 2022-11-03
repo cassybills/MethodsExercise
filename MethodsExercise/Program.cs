27d4cf876bf1b54a84611b0f516a781a1d699e66
cassybills <cassylynn92@gmail.com>
2022-11-03 19:02:31 -0400

    initial commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace MethodsExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MethodsExercise
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            string userAnimalName = Console.ReadLine();
+
+           
+            Console.WriteLine("What is your favorite band?");
+            string userBandName = Console.ReadLine();
+
+            Console.WriteLine($"There is a woman named {userName},her favorite color is {color}.");       
+            Console.WriteLine($"Her favorite animal is her {userAnimalName} that she loves more than anything in the universe.");
+            Console.WriteLine($"The only thing she loves almost as much as her dog is her favorite band {userBandName}.");
+
+
+            
+            
+
+            
+            
+
+           
+                       
+
     }
+            
+
+        
+    
 }
